3f00a1a088a358ae482a8a2ad8d755dce99c7138
Abhay Singh <akanyaani@gmail.com>
2019-09-07 20:33:04 +0530

    Update attention_layer.py

--- layers/attention_layer.py
+++ layers/attention_layer.py
@@ -3,7 +3,7 @@ from layers.feed_forward import Conv1d
 
 
 class MultiHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, att_dropout=0.1, residual_dropout=0.1, scale=False):
+    def __init__(self, d_model, num_heads, att_dropout=0.1, residual_dropout=0.1, scale=True):
         super(MultiHeadAttention, self).__init__()
         self.num_heads = num_heads
         self.d_model = d_model

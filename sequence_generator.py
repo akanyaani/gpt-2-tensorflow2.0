983c2409193048919ae558efcb71b4d676a10c0b
abhay kumar <akanyaani@gmail.com>
2020-01-26 18:13:06 +0530

    gpt2 seq gen code updated

--- sequence_generator.py
+++ sequence_generator.py
@@ -4,26 +4,25 @@ import click
 
 @click.command()
 @click.option('--model-path', type=str, default="./model", show_default=True, help="Model Path")
-@click.option('--model-parm', type=str, default="./model", show_default=True, help="Model Parm")
-@click.option('--vocab', type=str, default="./model", show_default=True, help="Vocab")
+@click.option('--model-param', type=str, default="./model/model_par.json", show_default=True, help="Model Parm")
+@click.option('--vocab', type=str, default="./data/bpe_model.model", show_default=True, help="Vocab")
 @click.option('--seq-len', type=int, default=512, show_default=True, help="seq_len")
 @click.option('--temperature', type=float, default=1.0, show_default=True, help="seq_len")
-@click.option('--top-k', type=int, default=512, show_default=True, help="seq_len")
-@click.option('--top-p', type=int, default=512, show_default=True, help="seq_len")
-@click.option('--nucleus_sampling', type=int, default=512, show_default=True, help="seq_len")
-@click.option('--context', type=str, default="", show_default=True, help="Context given to model")
+@click.option('--top-k', type=int, default=8, show_default=True, help="seq_len")
+@click.option('--top-p', type=float, default=0.9, show_default=True, help="seq_len")
+@click.option('--nucleus_sampling', type=bool, default=False, show_default=True, help="seq_len")
+@click.option('--context', type=str, default="sample context", show_default=True, help="Context given to model")
 def seq_gen(model_path, model_param, vocab, seq_len, temperature, top_k, top_p, nucleus_sampling, context):
-    sg = SequenceGenerator(model_path, model_param, vocab)
-    sg.load_weights()
-    gerated_seq = sg.sample_sequence(context,
-                                     seq_len=512,
-                                     temperature=1,
-                                     top_k=8,
-                                     top_p=0.9,
-                                     nucleus_sampling=True)
-
-    print(gerated_seq)
+	sg = SequenceGenerator(model_path, model_param, vocab)
+	sg.load_weights()
+	generated_seq = sg.sample_sequence(context,
+									   seq_len=seq_len,
+									   temperature=temperature,
+									   top_k=top_k,
+									   top_p=top_p,
+									   nucleus_sampling=nucleus_sampling)
+	print("Generated seq by model:- " + generated_seq)
 
 
 if __name__ == "__main__":
-    seq_gen()
+	seq_gen()
